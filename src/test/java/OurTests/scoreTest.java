115a10ddc651303c9fd9fd9a030c3a494c19b042
Lamarrrk <londonbridge1602@gmail.com>
2024-06-08 17:18:00 +0300

    Add first test of our project

---
+++ src/test/java/OurTests/scoreTest.java
@@ -0,0 +1,41 @@
+package OurTests;
+
+import Main.Game;
+import Main.GameObject;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class scoreTest {
+
+    private Game game;
+
+    @Before
+    public void setUp() {
+        game = new Game();
+    }
+
+    @Test
+    public void testScoreIncreaseWhenMerging() {
+        Game.objects.clear();
+        Game.objects.add(new GameObject(2, 2));
+        Game.objects.add(new GameObject(2, 2));
+
+        game.checkForValueIncrease();
+
+        assertEquals(4, game.getScore());
+    }
+
+    @Test
+    public void testScoreNotIncreasedWhenNoMerge() {
+        Game.objects.clear();
+        Game.objects.add(new GameObject(1, 2));
+        Game.objects.add(new GameObject(5, 9));
+
+        game.checkForValueIncrease();
+
+        assertEquals(0, game.getScore());
+    }
+}
+
